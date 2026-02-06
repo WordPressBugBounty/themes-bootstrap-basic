8470e948f3d2703e9194fc3529066836bed31872
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:39:23 +0000

    1.3.1

--- bootstrap-basic/functions.php
+++ bootstrap-basic/functions.php
@@ -185,13 +185,6 @@ if (!function_exists('bootstrapBasicEnqueueScripts')) {
 
         // js that is useful for development.
         wp_enqueue_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.min.js', array(), '3.6.0-20190314', true);
-        // js that is useful for old browsers.
-        wp_register_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js', array(), '1.4.2', true);
-        $wp_scripts->add_data('respond-script', 'conditional', 'lt IE 9');
-        wp_enqueue_script('respond-script');
-        wp_register_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.min.js', array(), '3.7.3', true);
-        $wp_scripts->add_data('html5-shiv-script', 'conditional', 'lte IE 9');
-        wp_enqueue_script('html5-shiv-script');
         
         if (is_singular() && get_option('thread_comments')) {
             wp_enqueue_script('comment-reply');
